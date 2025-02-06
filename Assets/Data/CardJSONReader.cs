6700a31678d938be976a8ddcbfff4c143c74160c
jgillooly <122410857+jgillooly@users.noreply.github.com>
2025-02-06 09:36:24 -0700

    Updated Various Things

--- Assets/Data/CardJSONReader.cs
+++ Assets/Data/CardJSONReader.cs
@@ -10,11 +10,14 @@ using Newtonsoft.Json;
 
 public class CardJSONReader : MonoBehaviour
 {
+    public GameObject cardPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         TextAsset cards = Resources.Load<TextAsset>("cards");
         Dictionary<string, List<CardDataBase>> cardDictionary = JsonConvert.DeserializeObject<Dictionary<string, List<CardDataBase>>>(cards.text);
+        var CurrentCard = Instantiate(cardPrefab);
+        CurrentCard.GetComponent<CardData>().Instantiate(cardDictionary["cards"][0]);
     }
 
     // Update is called once per frame
