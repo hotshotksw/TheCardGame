eb09b9c6070c7a2245a58a17eda3a8259476b9d3
jgillooly <122410857+jgillooly@users.noreply.github.com>
2025-03-12 09:04:06 -0600

    Added proper rarity percentages

--- Assets/Scripts/Pack.cs
+++ Assets/Scripts/Pack.cs
@@ -9,7 +9,12 @@ public class Pack : MonoBehaviour
     Dictionary<string, List<CardDataBase>> cardDictionary;
     [SerializeField] List<int> PullList;
     [SerializeField] Inventory inventory;
-    
+    float CommonChance = 60.0f;
+    float UncommonChance = 25.0f;
+    float RareChance = 9.0f;
+    float SuperRareChance = 5.0f;
+    float MythicalChance = 1.0f;
+
     void Start()
     {
         inventory = GameObject.Find("Inventory").GetComponent<Inventory>();
@@ -47,11 +52,7 @@ public class Pack : MonoBehaviour
 
     public void GetOneCard(GameManager_OLD.SceneCard card)
     {
-        string rarity = ((CardRarity)UnityEngine.Random.Range(0, Enum.GetValues(typeof(CardRarity)).Length)).ToString();
-        while (cardDictionary[rarity].Count == 0)
-        {
-            rarity = ((CardRarity)UnityEngine.Random.Range(0, Enum.GetValues(typeof(CardRarity)).Length)).ToString();
-        }
+        string rarity = getRarity();
 
         int random = UnityEngine.Random.Range(0, cardDictionary[rarity].Count);
         while(cardDictionary[rarity][random] == null)
@@ -79,5 +80,52 @@ public class Pack : MonoBehaviour
         {
             GetOneCard(cards[i]);
         }
+    }
+
+    private string getRarity()
+    {
+        string rarity = "";
+        if ((CommonChance + UncommonChance + RareChance + SuperRareChance + MythicalChance) != 100.0f)
+        {
+            rarity = ((CardRarity)UnityEngine.Random.Range(0, Enum.GetValues(typeof(CardRarity)).Length)).ToString();
+            while (cardDictionary[rarity].Count == 0)
+            {
+                rarity = ((CardRarity)UnityEngine.Random.Range(0, Enum.GetValues(typeof(CardRarity)).Length)).ToString();
+            }
+        }
+        else
+        {
+            float CommonCeiling = CommonChance;
+            float UncommonCeiling = CommonCeiling + UncommonChance;
+            float RareCeiling = UncommonCeiling + RareChance;
+            float SuperRareCeiling = RareCeiling + SuperRareChance;
+            float MythicalCeiling = SuperRareCeiling + MythicalChance;
+            float selection = UnityEngine.Random.Range(0.0f, 100.0f);
+            if (selection > 0 && selection <= CommonCeiling)
+            {
+                return "Common";
+            }
+            else if (selection > CommonCeiling && selection <= UncommonCeiling)
+            {
+                return "Uncommon";
+            }
+            else if (selection > UncommonCeiling && selection <= RareCeiling)
+            {
+                return "Rare";
+            }
+            else if (selection > RareCeiling && selection <= SuperRareCeiling)
+            {
+                return "SuperRare";
+            }
+            else if (selection > SuperRareCeiling && selection <= MythicalCeiling)
+            {
+                return "Mythical";
+            }
+            return "Something Broke";
+        }
+
+
+
+        return rarity;
     }
 }
